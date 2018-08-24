523511921b873a3aab956d21fac1ac1c6b63b8fb
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-08-24 09:05:20 -0400

    allowing NUM_PY_THREADS as argument

--- run_it.sh
+++ run_it.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-NUM_PY_THREADS=4
+if [[ $# -eq 0 ]] ; then
+	NUM_PY_THREADS=1
+elif [[ $# -eq 1 ]] ; then
+	NUM_PY_THREADS=$1
+else
+	echo 'Wrong number of parameters passed to xml_rewrite: only NUM_PY_THREADS needed.'
+    exit 1
+fi
+
 python -m src $NUM_PY_THREADS
 rm -rf unzipped_files/*
