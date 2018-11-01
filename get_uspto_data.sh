ce2019006639e69441a3259a11b76e5c8e36c1f7
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-11-01 10:11:31 -0400

    initial count variable wrong

--- get_uspto_data.sh
+++ get_uspto_data.sh
@@ -2,7 +2,7 @@
 
 for YEAR in {1976..2001}
 do
-    COUNT=`ls GBD_1976_2001_dat_to_xml/inData/"$YEAR".zip 2> /dev/null | wc -l`
+    COUNT=`ls dat_files/"$YEAR".zip 2> /dev/null | wc -l`
     while [ "$COUNT" -lt 1 ]; do
         `wget -q -r -l1 -nd -P ./dat_files/ --no-parent -A "$YEAR.zip" "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
         COUNT=`ls dat_files/"$YEAR".zip 2> /dev/null | wc -l`
