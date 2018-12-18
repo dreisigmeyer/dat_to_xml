c1c416c18ea068b9945d5ad1491ed4af6d684384
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-12-18 12:14:48 -0500

    updating count

--- get_uspto_data.sh
+++ get_uspto_data.sh
@@ -5,9 +5,9 @@
 PATH_TO_DAT=`dirname $(readlink -f $0)`
 for YEAR in {1976..2001}
 do
-    COUNT=`ls dat_files/"$YEAR".zip 2> /dev/null | wc -l`
+    COUNT=`ls "$PATH_TO_DAT"/dat_files/"$YEAR".zip 2> /dev/null | wc -l`
     while [ "$COUNT" -lt 1 ]; do
         `wget -q -r -l1 -nd -P "$PATH_TO_DAT"/dat_files/ --no-parent -A "$YEAR.zip" "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/$YEAR/"`
-        COUNT=`ls dat_files/"$YEAR".zip 2> /dev/null | wc -l`
+        COUNT=`ls "$PATH_TO_DAT"/dat_files/"$YEAR".zip 2> /dev/null | wc -l`
     done
 done
